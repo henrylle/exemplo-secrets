fcc400f387b0dc9ac22caab6399c4f75f240c83b
Henrylle Maia <henryllemaia@gmail.com>
2024-02-06 16:52:54 -0300

    ajuste na conexao

--- teste_conexao_psql.sh
+++ teste_conexao_psql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-secret_string=$(aws secretsmanager get-secret-value --secret-id rds!db-e8cb0dc4-d321-4af4-854a-2b1e75cb55d9 --version-stage AWSCURRENT --query SecretString --output text --profile secrets)
+secret_string=$(aws secretsmanager get-secret-value --secret-id rds!db-e8cb0dc4-d321-4af4-854a-2b1e75cb55d9 --version-stage AWSCURRENT --query SecretString --output text)
 username=$(echo $secret_string | jq -r '.username')
 password=$(echo $secret_string | jq -r '.password')
 
@@ -12,4 +12,4 @@ DB_USER=$username
 DB_PASSWORD=$password
 
 # Comando psql para se conectar e executar uma consulta SQL
-PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -c "SUA_QUERY_SQL_AQUI"
+PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -c "SELECT schema_name FROM information_schema.schemata"
